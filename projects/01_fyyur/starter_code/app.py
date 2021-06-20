9d926460420edbb307d2afa844a284541e95d22c
abdullah <s437036261@st.uqu.edu.sa>
2021-06-20 14:20:03 +0300

    enable the search on artists names

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -276,15 +276,13 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+    search_term = request.form.get('search_term', '')
+    data = Artist.query.filter(Artist.name.like("%{0}%".format(search_term))).all()
+    result = {
+        "count": len(data),
+        "data": data
     }
-    return render_template('pages/search_artists.html', results=response,
+    return render_template('pages/search_artists.html', results=result,
                            search_term=request.form.get('search_term', ''))
 
 
