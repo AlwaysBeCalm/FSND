d2742d2875ebc8b09607d71b5de3baefb54b7302
abdullah <s437036261@st.uqu.edu.sa>
2021-06-20 14:14:58 +0300

    Enable the delete venue functionality, using fetch (javascript)

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -7,7 +7,7 @@ from logging import Formatter, FileHandler
 
 import babel
 import dateutil.parser
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -248,11 +248,18 @@ def create_venue_submission():
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
     # TODO: Complete this endpoint for taking a venue_id, and using
-    # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
-
+    # TODO: SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail. (Done)
+    try:
+        Venue.query.filter_by(id=venue_id).delete()
+        db.session.commit()
+        return jsonify({'success': True})
+    except:
+        db.session.rollback()
+        return jsonify({'success': False})
+    finally:
+        db.session.close()
     # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-    # clicking that button delete it from the db then redirect the user to the homepage
-    return None
+    # clicking that button delete it from the db then redirect the user to the homepage (Done)
 
 
 #  Artists
