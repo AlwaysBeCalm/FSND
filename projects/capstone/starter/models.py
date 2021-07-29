6f22aa40c9cdeadde3894259a31eaba859363feb
abdullah <s437036261@st.uqu.edu.sa>
2021-07-30 00:39:38 +0300

    the models/tables of the library system.

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -5,13 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Author(db.Model):
-    __tablename__ = 'Author'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-
-    # TODO: implement one-to-many relationship between Author and Book (DONE)
-    books = db.relationship('Book', backref='author')
+class DBOperations(db.Model):
+    __abstract__ = True
 
     def insert(self):
         try:
@@ -38,6 +33,15 @@ class Author(db.Model):
             db.session.rollback()
         finally:
             db.session.close()
+
+
+class Author(DBOperations):
+    __tablename__ = 'Author'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # TODO: implement one-to-many relationship between Author and Book (DONE)
+    books = db.relationship('Book', backref='author')
 
     def format(self):
         return {
@@ -47,39 +51,13 @@ class Author(db.Model):
         }
 
 
-class Category(db.Model):
+class Category(DBOperations):
     __tablename__ = 'Category'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False, unique=True)
 
     # TODO: implement one-to-many relationship between Category and Book (DONE)
     books = db.relationship('Book', backref='category')
-
-    def insert(self):
-        try:
-            db.session.add(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def update(self):
-        try:
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def delete(self):
-        try:
-            db.session.delete(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
 
     def format(self):
         return {
@@ -89,7 +67,7 @@ class Category(db.Model):
         }
 
 
-class Book(db.Model):
+class Book(DBOperations):
     __tablename__ = 'Book'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
@@ -97,34 +75,10 @@ class Book(db.Model):
     pages = db.Column(db.Integer, nullable=False)
     about = db.Column(db.String(255), nullable=False)
 
-    author_id = db.Column('author_id', db.Integer, db.ForeignKey('Author.id', ondelete='CASCADE'),)
-    category_id = db.Column('category_id', db.Integer, db.ForeignKey('Category.id', ondelete='CASCADE'),)
-
-    def insert(self):
-        try:
-            db.session.add(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def update(self):
-        try:
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def delete(self):
-        try:
-            db.session.delete(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
+    author_id = db.Column('author_id', db.Integer, db.ForeignKey(
+        'Author.id', ondelete='CASCADE'),)
+    category_id = db.Column('category_id', db.Integer,
+                            db.ForeignKey('Category.id', ondelete='CASCADE'),)
 
     # TODO: add rating here (Done)
     def get_rating(self):
@@ -132,7 +86,8 @@ class Book(db.Model):
         for book in self.borrowed:
             ratings = BorrowedBooks.query.get(book.id)
             current_book_rating += ratings.rating
-        avg_rating = current_book_rating / (len(self.borrowed) if len(self.borrowed) != 0 else 1)
+        avg_rating = current_book_rating / \
+            (len(self.borrowed) if len(self.borrowed) != 0 else 1)
         return avg_rating
 
     def format(self):
@@ -149,36 +104,10 @@ class Book(db.Model):
         }
 
 
-class Borrower(db.Model):
+class Borrower(DBOperations):
     __tablename__ = 'Borrower'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-
-    def insert(self):
-        try:
-            db.session.add(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def update(self):
-        try:
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def delete(self):
-        try:
-            db.session.delete(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
 
     def format(self):
         return {
@@ -188,10 +117,12 @@ class Borrower(db.Model):
         }
 
 
-class BorrowedBooks(db.Model):
+class BorrowedBooks(DBOperations):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    book_id = db.Column('book_id', db.Integer, db.ForeignKey('Book.id', ondelete='CASCADE'))
-    borrower_id = db.Column('borrower_id', db.Integer, db.ForeignKey('Borrower.id', ondelete='CASCADE'))
+    book_id = db.Column('book_id', db.Integer, db.ForeignKey(
+        'Book.id', ondelete='CASCADE'))
+    borrower_id = db.Column('borrower_id', db.Integer,
+                            db.ForeignKey('Borrower.id', ondelete='CASCADE'))
     borrowed_at = db.Column(db.DateTime, default=datetime.datetime.now())
 
     # TODO: implement rating when a borrower returns a book (Done)
@@ -200,32 +131,6 @@ class BorrowedBooks(db.Model):
 
     borrower = db.relationship('Borrower', backref='borrower')
     borrowed = db.relationship('Book', backref='borrowed')
-
-    def insert(self):
-        try:
-            db.session.add(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def update(self):
-        try:
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-
-    def delete(self):
-        try:
-            db.session.delete(self)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
 
     def format(self):
         return {
